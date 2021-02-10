4b60ac22eee72e62a263107c769e9ee26c434871
Clay Tomerlin <ctomerlin@salesforce.com>
2021-02-10 13:50:07 -0800

    placeholder test for pytest pass

---
+++ dev/tasks/tests/test_community_templates.py
@@ -0,0 +1,9 @@
+# from unittest.mock import Mock
+# from unittest import TestCase
+# from cumulusci.tasks.salesforce.tests.util import create_task
+# from dev.tasks.community_templates import ExportCommunityTemplateTask
+
+
+class TestExportCommunityTemplateTask:
+    def placeholder(self):
+        pass
