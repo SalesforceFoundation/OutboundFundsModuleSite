38c9b016cf7f0a20aab9cb37d58d7b000b0fe85a
Stevie Ella <sella@salesforce.com>
2021-02-17 11:33:22 -0600

    W-8715476: Add OFM Community template (#2)
    * Add OFM Community template
    
    * Add content asset
    
    * Add content assets for logo
    
    * Update bundle contents
    
    * Add communities to org shapes
    
    * Add communities.py
    
    * python format fixes
    
    * updated reqs for pytest and rm unused test
    
    * placeholder test for pytest pass
    
    * cleaned up workflows and python test
    
    * jest workflow modify
    
    * Remove unused static resources.
    
    * Bringing pytests over for custom tasks
    
    - Copied `dev/tasks/tests` from Grants Management.
    - Re-added pyest Github Action.
    
    * Re-adding pytest Github Action
    
    * Pytest for dev.tasks.debugger.log_title
    
    Co-authored-by: Clay Tomerlin <ctomerlin@salesforce.com>
    Co-authored-by: Scott Pelak <spelak@salesforce.com>

---
+++ dev/tasks/debugger.py
@@ -0,0 +1,294 @@
+"""
+Example implementing debug in a CumulusCI Task:
+
+def _debug(self, value, title=None, show_list_index=True):
+    debug(
+        value, title=title, show_list_index=show_list_index, logger=self.logger.info
+    )
+"""
+
+
+def debug(
+    value,
+    title=None,
+    indent=0,
+    tab="  ",
+    show_list_index=True,
+    logger=print,
+):
+    indentation = tab * (indent)
+    prefix = "" if title is None else str(title) + " : "
+    if isinstance(value, dict):
+        logger(indentation + prefix + "{")
+        for key, dict_value in value.items():
+            debug(
+                dict_value,
+                title=key,
+                indent=(indent + 1),
+                tab=tab,
+                show_list_index=show_list_index,
+                logger=logger,
+            )
+        logger(indentation + "}" + ("," if indent else ""))
+    elif isinstance(value, list):
+        logger(indentation + prefix + "[")
+        for index, list_item in enumerate(value):
+            title = index if show_list_index else None
+            debug(
+                list_item,
+                title=title,
+                indent=(indent + 1),
+                tab=tab,
+                show_list_index=show_list_index,
+                logger=logger,
+            )
+        logger(indentation + "]" + ("," if indent else ""))
+    elif isinstance(value, set):
+        logger(indentation + prefix + "set(")
+        for index, list_item in enumerate(value):
+            debug(
+                list_item,
+                title=index,
+                indent=(indent + 1),
+                tab=tab,
+                show_list_index=show_list_index,
+                logger=logger,
+            )
+        logger(indentation + ")" + ("," if indent else ""))
+    else:
+        logger(indentation + prefix + str(value) + ("," if indent else ""))
+
+
+"""
+Example implementing log_title in a CumulusCI Task:
+
+    def _log_title(self, title):
+        log_title(
+            title, logger=self.logger.info
+        )
+"""
+
+
+def log_title(title, logger=print):
+    if title:
+        logger("")
+        logger(title)
+        logger("─" * len(title))
+
+
+def _get_padded_log_columns(column_max_lengths, column_range, row):
+    log_row = []
+    for index in column_range:
+        column = row[index] if index < len(row) else ""
+        log_row.append(
+            "{}{}".format(column, " " * (column_max_lengths[index] - len(column)))
+        )
+    return log_row
+
+
+def _get_log_column_separators(
+    columnBorders=True,
+    padColumns=True,
+):
+    column_separators = {
+        "value": "{}",
+        "space": " " if padColumns else "",
+        "pad─": "─" if padColumns else "",
+        "┌": "┌" if columnBorders else "─",
+        "┐": "┐" if columnBorders else "─",
+        "└": "└" if columnBorders else "─",
+        "┘": "┘" if columnBorders else "─",
+        "├": "├",
+        "┼": "┼",
+        "┤": "┤",
+        "┬": "┬",
+        "┴": "┴",
+        "│": "│",
+    }
+
+    if not columnBorders:
+        column_separators.update({"├": "", "┼": "", "┤": "", "┬": "", "┴": "", "│": ""})
+
+    return column_separators
+
+
+def _get_table_borders(
+    column_max_lengths,
+    columnBorders=True,
+    padColumns=True,
+):
+    table_borders = {}
+    table_borders["borders"] = []
+
+    column_separators = _get_log_column_separators(columnBorders, padColumns)
+
+    for i in range(0, len(column_max_lengths) + 1):
+        columns = []
+        for index, length in enumerate(column_max_lengths):
+            columns.append((" " if index < i else "─") * length)
+
+        if i == 0:
+            border_format = "".join(
+                [
+                    "{├}{pad─}{value}{pad─}".format(**column_separators),
+                    ("{┼}{pad─}{value}{pad─}" * (len(column_max_lengths) - 1)).format(
+                        **column_separators
+                    ),
+                    "{┤}".format(**column_separators),
+                ]
+            )
+
+            table_borders["header_border"] = "".join(
+                [
+                    "{├}{pad─}{value}{pad─}".format(**column_separators),
+                    ("{┼}{pad─}{value}{pad─}" * (len(column_max_lengths) - 1)).format(
+                        **column_separators
+                    ),
+                    "{┤}".format(**column_separators),
+                ]
+            ).format(*columns)
+
+            if columnBorders:
+                table_borders["header"] = "".join(
+                    [
+                        "{┌}{pad─}{value}{pad─}".format(**column_separators),
+                        (
+                            "{┬}{pad─}{value}{pad─}" * (len(column_max_lengths) - 1)
+                        ).format(**column_separators),
+                        "{┐}".format(**column_separators),
+                    ]
+                ).format(*columns)
+
+                table_borders["footer"] = "".join(
+                    [
+                        "{└}{pad─}{value}{pad─}".format(**column_separators),
+                        (
+                            "{┴}{pad─}{value}{pad─}" * (len(column_max_lengths) - 1)
+                        ).format(**column_separators),
+                        "{┘}".format(**column_separators),
+                    ]
+                ).format(*columns)
+            else:
+                table_borders["header"] = table_borders["header_border"]
+                table_borders["footer"] = table_borders["header_border"]
+
+        elif i < len(column_max_lengths):
+            border_format = "".join(
+                [
+                    ("{│}{space}{value}{space}" * i).format(**column_separators),
+                    "{├}{pad─}{value}{pad─}".format(**column_separators),
+                    (
+                        "{┼}{pad─}{value}{pad─}" * (len(column_max_lengths) - 1 - i)
+                    ).format(**column_separators),
+                    "{┤}".format(**column_separators),
+                ]
+            )
+        else:
+            border_format = "".join(
+                [
+                    ("{│}{space}{value}{space}" * len(column_max_lengths)).format(
+                        **column_separators
+                    ),
+                    "{│}".format(**column_separators),
+                ]
+            )
+            table_borders["row_format"] = border_format
+
+        table_borders["borders"].append(border_format.format(*columns))
+
+    return table_borders
+
+
+def _format_log_rows(raw_rows):
+    formatted_rows = {}
+    formatted_rows["rows"] = []
+    column_max_lengths = []
+    formatted_rows["column_max_lengths"] = column_max_lengths
+
+    if raw_rows:
+        for raw_row in raw_rows:
+            row = []
+            formatted_rows["rows"].append(row)
+            for index, raw_column in enumerate(raw_row):
+                column = str(raw_column) if raw_column else ""
+                row.append(column)
+                if index < len(column_max_lengths):
+                    column_max_lengths[index] = max(
+                        column_max_lengths[index], len(column)
+                    )
+                else:
+                    column_max_lengths.append(len(column))
+    return formatted_rows
+
+
+"""
+Example implementing log_table in a CumulusCI Task:
+
+    def _log_table(
+        self,
+        raw_rows,
+        groupByBlankColumns=False,
+        rowBorders=True,
+        columnBorders=True,
+        padColumns=True,
+        addHeaderBorder=True,
+    ):
+        log_table(
+            raw_rows,
+            groupByBlankColumns=False,
+            rowBorders=True,
+            columnBorders=True,
+            padColumns=True,
+            addHeaderBorder=True,
+            logger=self.logger.info
+        )
+"""
+
+
+def log_table(
+    raw_rows,
+    groupByBlankColumns=False,
+    rowBorders=True,
+    columnBorders=True,
+    padColumns=True,
+    addHeaderBorder=True,
+    logger=print,
+):
+    formatted_rows = _format_log_rows(raw_rows)
+    rows = formatted_rows.get("rows")
+    column_max_lengths = formatted_rows.get("column_max_lengths")
+
+    if column_max_lengths:
+        column_range = range(0, len(column_max_lengths))
+
+        table_borders = _get_table_borders(
+            column_max_lengths,
+            columnBorders,
+            padColumns,
+        )
+
+        logger(table_borders.get("header"))
+
+        # last_group_index = -1
+        for index, row in enumerate(rows):
+            # log border
+            if addHeaderBorder and index == 1:
+                logger(table_borders.get("header_border"))
+            elif rowBorders and 0 < index:
+                border_index = 0
+                if groupByBlankColumns:
+                    border_index = len(column_max_lengths)
+                    for i in column_range:
+                        if row[i]:
+                            border_index = i
+                            break
+                logger(table_borders.get("borders")[border_index])
+
+            # log row
+            logger(
+                table_borders.get("row_format").format(
+                    *_get_padded_log_columns(column_max_lengths, column_range, row)
+                )
+            )
+
+        logger(table_borders.get("footer"))
---
+++ dev/tasks/namespaces.py
@@ -0,0 +1,167 @@
+import os
+import json
+from cumulusci.utils import inject_namespace
+from cumulusci.core.utils import process_list_arg
+from cumulusci.core.exceptions import CumulusCIException
+from cumulusci.core.tasks import BaseTask
+from dev.tasks.debugger import log_title
+from cumulusci.core.config import TaskConfig
+from cumulusci.tasks.salesforce import GetInstalledPackages
+
+
+def get_default_package_directory():
+    with open("sfdx-project.json", "r") as f:
+        project = json.load(f)
+    for source_path in project["packageDirectories"]:
+        if source_path.get("default"):
+            return source_path["path"]
+    raise CumulusCIException(
+        'One of of sfdx-project.json "packageDirectories" must be marked as default.'
+    )
+
+
+def _is_project_package_installed(self: BaseTask):
+    """
+    Temporary fix to detect if the org is in a "managed" context meaning
+    the project's package is installed.
+
+    The plan is to use self.org_config.has_minimum_version() when released.
+    """
+    namespace = self.project_config.project__package__namespace
+    for package in GetInstalledPackages(
+        self.project_config,
+        TaskConfig({}),
+        self.org_config,
+    )().items():
+        installed_package_namespace = package[0]
+        if namespace == installed_package_namespace:
+            return True
+    return False
+
+
+def get_task_namespace_info(self: BaseTask):
+    """
+    Retrieves useful calculations of whether or not namespace injection is needed.
+    """
+    # Always use the project's package's namespace as the namespace to inject.
+    is_packaged_installed = False
+    namespace = self.project_config.project__package__namespace
+
+    # namespaced_org is true if the scratch org is a namespaced org.
+    is_namespaced_org = self.org_config.scratch and self.org_config.namespaced
+
+    # is_managed passed in as the "managed" kwarg for inject_namespace().
+    # is_managed is true if any of the following is true:
+    # - is a namespaced org
+    # - this package is installed
+    if not is_namespaced_org:
+        is_packaged_installed = _is_project_package_installed(self)
+
+    is_managed = is_namespaced_org or is_packaged_installed
+
+    return {
+        "namespace": namespace,
+        "is_namespaced_org": is_namespaced_org,
+        "is_packaged_installed": is_packaged_installed,  # is_packaged_installed is not checked if is_namespaced_org is True
+        "is_managed": is_managed,
+    }
+
+
+class InjectNamespaceBaseTask(BaseTask):
+    task_options = {
+        "ignore_directories_for_inject_namespace": {
+            "description": 'Directories which inject_namespace will not process.  Option must be an array.  Always ignores directories starting with "." or "_", and always ignores file names starting with ".".  Default: [contentassets, staticresources].',
+            "required": False,
+        },
+    }
+
+    def _ignore_inject_namespace_for_directory(self, directory):
+        return directory.startswith((".", "_"))
+
+    def _ignore_inject_namespace_for_file(self, file_name):
+        return file_name.startswith(
+            (
+                ".",
+                "sfdx-project.json",
+            )
+        )
+
+    def _inject_namespace(
+        self,
+        source_path: str,
+        managed=False,
+    ):
+        """
+        Walks through source_path and calls inject_namespace on files whose:
+        - Directory or parent directory:
+            - Is not in "ignore_directories_for_inject_namespace" option
+            - Does not start with:
+                - "." for configuration directories
+                - "_" for test directories
+        - File name starts with:
+            - "." for configuration files
+            - "sfdx-project.json" since it's a configuration file.
+
+        NOTE: We pass in the "namespace", "managed", and "namespaced_org" kwargs into
+              inject_namespace() to handle most (if not all) namespace tokens and
+              contexts.
+        """
+
+        # Set which additional directories to ignore that make it pass _ignore_inject_namespace_for_directory().
+        directories_to_ignore = set()
+        directories_to_ignore.update(
+            process_list_arg(
+                self.options.get("ignore_directories_for_inject_namespace")
+                or ["contentassets", "staticresources"]
+            )
+        )
+
+        # Walk through source_path and inject_namespace for all directories and files not ignored.
+        log_title("Injecting namespace in temporary directory", self.logger.info)
+        self.logger.info(source_path)
+
+        namespace_info = get_task_namespace_info(self)
+        for path, subdirectories, files in os.walk(source_path):
+            directory = os.path.basename(path)
+
+            if (
+                directory in directories_to_ignore
+                or self._ignore_inject_namespace_for_directory(directory)
+            ):
+                # directory is excluded. Exclude subdirectories too.
+                subdirectories[:] = []
+                continue
+
+            for file_name in files:
+                file_path = os.path.join(path, file_name)
+
+                if self._ignore_inject_namespace_for_file(file_name):
+                    continue
+
+                # Read file's content.
+                # TODO: BEWARE: This will do newline translation.  Come back to this.
+                with open(file_path, "r") as f:
+                    file_content = f.read()
+
+                # Inject namespace into file_name and file_content.
+                new_file_name, injected_file_content = inject_namespace(
+                    file_name,
+                    file_content,
+                    namespace=namespace_info["namespace"],
+                    # managed kwarg overrides namespace_info["is_managed"]
+                    managed=(managed or namespace_info["is_managed"]),
+                    namespaced_org=namespace_info["is_namespaced_org"],
+                    logger=self.logger,
+                )
+
+                # Write injected_file_content if inject_namespace() changes either file_name or file_content.
+                if new_file_name != file_name or injected_file_content != file_content:
+                    # If file_name changes after injecting namespace, remove the old file.
+                    if new_file_name != file_name:
+                        os.remove(file_path)
+
+                    # Save the injected_file_content.
+                    with open(os.path.join(path, new_file_name), "w") as f:
+                        f.write(injected_file_content)
+
+        self.logger.info("")
