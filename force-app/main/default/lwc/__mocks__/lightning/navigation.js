b0c126429f5b1d3bb634ee6bd2f2f885d8e94cfc
Julio Davila <59618328+jdavilasf@users.noreply.github.com>
2021-03-08 15:17:36 -0500

    [W-8789244] - Apply to Funding Program (#13)
    * Adding Apply Button components
    
    * updating flows location and removing js logs
    
    * Adding namespace prefix to flow
    
    * Adding story telling data and OFM FP flexipage
    
    * Removing GMS Permset reference
    
    * Updating Custom labels based on Docs review
    
    * Removing Aura component and Adding Apply QuickAction
    - After a conversation with the team we determined that we shouldn't
      use components that are protected by the PSL on GMS. The aura
      component validates whether the users has the license. Instead the
      team suggested a quick action that fires the flow.
    
    * Updating flow based on review feedback.
    - The flow is triggered directly from the Apply quick action. Thanks
      to this there's not need to have the ApplicationForm field on the
      Funding Program object.
    - Flow was renamed to OFMSiteCreateApplication
    - Storytellling data changes were reverted.
    
    * Making fields required in flow
    
    Co-authored-by: salesforce-org-metaci[bot] <53311887+salesforce-org-metaci[bot]@users.noreply.github.com>

---
+++ force-app/main/default/lwc/__mocks__/lightning/navigation.js
@@ -0,0 +1,58 @@
+/**
+ * For the original lightning/navigation mock that comes by default with
+ * @salesforce/sfdx-lwc-jest, see:
+ * https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-stubs/navigation/navigation.js
+ */
+export const CurrentPageReference = jest.fn();
+
+const NAVIGATE_SYMBOL = "Navigate";
+const GENERATE_URL_SYMBOL = "GenerateUrl";
+
+let _navigatePageReference,
+    _generatePageReference,
+    _replace,
+    _url = "https://www.example.com";
+
+const Navigate = Symbol(NAVIGATE_SYMBOL);
+const GenerateUrl = Symbol(GENERATE_URL_SYMBOL);
+export const NavigationMixin = (Base) => {
+    return class extends Base {
+        [Navigate](pageReference, replace) {
+            _navigatePageReference = pageReference;
+            _replace = replace;
+        }
+        [GenerateUrl](pageReference) {
+            _generatePageReference = pageReference;
+            return new Promise((resolve) => resolve(_url));
+        }
+    };
+};
+NavigationMixin.Navigate = Navigate;
+NavigationMixin.GenerateUrl = GenerateUrl;
+
+/*
+ * Tests do not have access to the internals of this mixin used by the
+ * component under test so save a reference to the arguments the Navigate method is
+ * invoked with and provide access with this function.
+ */
+export const getNavigateCalledWith = () => {
+    return {
+        pageReference: _navigatePageReference,
+        replace: _replace,
+    };
+};
+
+export const getGenerateUrlCalledWith = () => ({
+    pageReference: _generatePageReference,
+});
+
+export const setGenerateUrl = (url) => {
+    _url = url;
+};
+
+export const resetNavigationMixinMocks = () => {
+    _navigatePageReference = undefined;
+    _generatePageReference = undefined;
+    _replace = undefined;
+    _url = "https://www.example.com";
+};
