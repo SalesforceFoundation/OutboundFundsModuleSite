be3d79fdfdce070a70eeeef4abdd3bc119fc671b
Julio Davila <jdavila@salesforce.com>
2021-04-05 19:58:31 -0400

    Adding License to Dev python files

--- dev/tasks/debugger.py
+++ dev/tasks/debugger.py
@@ -1,4 +1,12 @@
 """
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ *
+"""
+
+"""
 Example implementing debug in a CumulusCI Task:
 
 def _debug(self, value, title=None, show_list_index=True):
