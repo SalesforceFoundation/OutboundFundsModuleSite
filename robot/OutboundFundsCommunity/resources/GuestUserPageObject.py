37b22aa27d6b7d1da6f8992237e9681c5489e434
djun1209 <54905812+djun1209@users.noreply.github.com>
2021-07-13 13:10:21 -0700

    fix guest portal user page

--- robot/OutboundFundsCommunity/resources/GuestUserPageObject.py
+++ robot/OutboundFundsCommunity/resources/GuestUserPageObject.py
@@ -11,7 +11,7 @@ class CommunityHomePage(BaseOutboundFundsCommunityPage, HomePage):
         by verifying that the url contains '/fundingseekerportal'
         """
         self.selenium.wait_until_location_contains(
-            "/fundingseekerportal/s", message="Current page is not a Community Portal"
+            "/fundingseekerportal", message="Current page is not a Community Portal"
         )
 
     def click_down_arrow(self):
