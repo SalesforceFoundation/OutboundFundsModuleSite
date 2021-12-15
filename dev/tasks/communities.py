21dbfb3d293ed1b94d065c3082eaba7c6cbcd486
a-contreras <acontreras@salesforce.com>
2021-12-15 09:16:22 -0500

    modified communities.py

--- dev/tasks/communities.py
+++ dev/tasks/communities.py
@@ -1,60 +1,51 @@
-# * Copyright (c) 2021, salesforce.com, inc.
-# * All rights reserved.
-# * SPDX-License-Identifier: BSD-3-Clause
-# * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
-# *
-
-""""
-Steps:
-
-1) Retrieve Network, extract picassoSite (is the API Name of ExpBundle)
-2) Retrieve ExperienceBundle
-3) Update themes: loop over all *.json files in themes directory
-    - navigationMenuEditorRefresh
-
-"""
 import os
+from pathlib import Path
 import re
-from tempfile import mkstemp
-from shutil import move
 import xml.etree.ElementTree as ET
 from cumulusci.core.exceptions import CumulusCIException
-from os import path, fdopen, remove
 from dev.tasks.debugger import log_title
 from dev.tasks.sfdx import SfdxEtlWithNamespaceInjectionTask
 
 
-class ReplaceThemeLayoutNavigationMenuTask(SfdxEtlWithNamespaceInjectionTask):
-
+class ExperienceBundleTask(SfdxEtlWithNamespaceInjectionTask):
     task_options = {
         "network_name": {"description": ("Name of Network"), "required": True},
-        "navigation_menu": {
-            "description": "NavigationMenu API name that will replace the default Navigation Menus in the community theme layout.",
-            "required": True,
-        },
         **SfdxEtlWithNamespaceInjectionTask.task_options,
     }
 
+    def _sfdx_extract(self):
+        # Create default package directory in temporary directory
+        self._temporary_package_directory = os.path.join(
+            self._temporary_directory, self._default_package_directory
+        )
+
+        self._network_name = self.options.get("network_name")
+
+        # Retrieve the Network metadata.
+        self._retrieve_network()
+
+        # Extract the ExperienceBundle API Name from the Network metadata.
+        self._set_experience_bundle_name()
+
+        # Retrieve the ExperienceBundle metadata.
+        self._retrieve_experience_bundle()
+
+    def _sfdx_transform(self):
+        pass
+
+    def _sfdx_load(self):
+        self._deploy_experience_bundle()
+        self._publish_network()
+
     def _retrieve_network(self):
+        self._log_title("Retrieving Network metadata")
         result = self._sfdx(
-            [
-                "force:source:retrieve",
-                '--metadata="Network:{}"'.format(self.options.get("network_name")),
-            ],
+            ["force:source:retrieve", f'--metadata="Network:{self._network_name}"'],
             addTargetUsername=True,
         )
 
-        self.retrieved_file_paths = set()
-        for inbound_file in result["inboundFiles"]:
-            self.retrieved_file_paths.add(inbound_file["filePath"])
-
-        # raise CumulusCIException is no results found.
-        if not result["inboundFiles"]:
-            raise CumulusCIException(
-                'Metadata not found: "Network:{}"'.format(
-                    self.options.get("network_name")
-                )
-            )
+        # Check if there were any errors retrieveing metadata
+        self._assert_sfdx_retrieved_successfully(result)
 
     def _set_experience_bundle_name(self):
         """
@@ -62,10 +53,15 @@ class ReplaceThemeLayoutNavigationMenuTask(SfdxEtlWithNamespaceInjectionTask):
         2) Extract picassoSite with regex.
         3) set self._experience_bundle_name = picassoSite
         """
-        basepath = path.dirname(self._temporary_package_directory)
-        filepath = path.abspath(path.join(basepath, self.retrieved_file_paths.pop()))
+        network_path = os.path.join(
+            self._temporary_package_directory,
+            "main",
+            "default",
+            "networks",
+            f"{self._network_name}.network-meta.xml",
+        )
 
-        tree = ET.parse(filepath)
+        tree = ET.parse(network_path)
         root = tree.getroot()
 
         for elem in root.findall(
@@ -75,36 +71,29 @@ class ReplaceThemeLayoutNavigationMenuTask(SfdxEtlWithNamespaceInjectionTask):
 
         self._experience_bundle_name = picasso_site
 
+        self._log_title("Extracting ExperienceBundle API Name from Network metadata")
+        self.logger.info(f"ExperienceBundle: {self._experience_bundle_name}")
+
     def _retrieve_experience_bundle(self):
+        self._log_title("Retrieving ExperienceBundle metadata")
         result = self._sfdx(
             [
                 "force:source:retrieve",
-                '--metadata="ExperienceBundle:{}"'.format(self._experience_bundle_name),
+                f'--metadata="ExperienceBundle:{self._experience_bundle_name}"',
             ],
             addTargetUsername=True,
         )
 
-        self.theme_file_paths = set()
-
-        for item in result["inboundFiles"]:
-            full_name = item["fullName"]
-            match = self._experience_bundle_name + "/themes" in full_name
-            if match:
-                self.theme_file_paths.add(item["filePath"])
-
-        # raise CumulusCIException is no results found.
-        if not result["inboundFiles"]:
-            raise CumulusCIException(
-                'Metadata not found: "ExperienceBundle:{}"'.format(
-                    self.options.get(self._experience_bundle_name)
-                )
-            )
+        # Check if there were any errors retrieveing metadata
+        self._assert_sfdx_retrieved_successfully(result)
 
     def _deploy_experience_bundle(self):
+        self._log_title("Deploying ExperienceBundle metadata")
+
         result = self._sfdx(
             [
                 "force:source:deploy",
-                '--metadata="ExperienceBundle:{}"'.format(self._experience_bundle_name),
+                f'--metadata="ExperienceBundle:{self._experience_bundle_name}"',
             ],
             addTargetUsername=True,
         )
@@ -112,47 +101,28 @@ class ReplaceThemeLayoutNavigationMenuTask(SfdxEtlWithNamespaceInjectionTask):
         # raise CumulusCIException is no results found.
         if not result["deployedSource"]:
             raise CumulusCIException(
-                'Metadata not found: "ExperienceBundle:{}"'.format(
-                    self.options.get(self._experience_bundle_name)
-                )
+                f'Metadata not found: "ExperienceBundle:{self._experience_bundle_name}"'
             )
 
-    def _replace_theme_navigation_menus(self):
-        """
-        Replaces default navigation with navigation menu specified in the option navigation_menu
-        """
-        basepath = path.dirname(self._temporary_package_directory)
-        filepath = path.abspath(path.join(basepath, self.theme_file_paths.pop()))
-
-        fh, abs_path = mkstemp()
-        log_title(
-            "Adding navigation menu: {} to community pages".format(
-                self.options["navigation_menu"]
-            ),
-            self.logger.info,
+    def _publish_network(self):
+        self._log_title("Publishing Network")
+        self._sfdx(
+            ["force:community:publish", f'--name="{self._network_name}"'],
+            addTargetUsername=True,
         )
-        page_count = 0
-        with fdopen(fh, "w") as new_file:
-            with open(filepath) as old_file:
-                substitution = '"navigationMenuEditorRefresh" : "{}"'.format(
-                    self.options["navigation_menu"]
-                )
-                for line in old_file:
-                    updated_line = re.sub(
-                        r'("navigationMenuEditorRefresh" ?: ?.*$)',
-                        substitution,
-                        line,
-                    )
-                    new_file.write(updated_line)
-                    if updated_line != line:
-                        page_count += 1
 
-        self.logger.info("Number of Community Pages updated: {}".format(page_count))
+    def _assert_sfdx_retrieved_successfully(self, sfdx_result):
+        errors = []
+        for inbound_file in sfdx_result["inboundFiles"]:
+            if inbound_file.get("error"):
+                errors.append(inbound_file.get("error"))
 
-        # Remove original file
-        remove(filepath)
-        # Move new file
-        move(abs_path, filepath)
+        if errors:
+            raise CumulusCIException("; ".join(errors))
+
+    def _log_title(self, title):
+        self.logger.info("")
+        log_title(title, self.logger.info)
 
     def _debug_temporary_directory(self):
         self.logger.warn("debugging temporary package directory")
@@ -160,18 +130,52 @@ class ReplaceThemeLayoutNavigationMenuTask(SfdxEtlWithNamespaceInjectionTask):
         self.logger.warn(f"temporary directory: {self._temporary_directory}")
         self.list_files(self._default_package_directory)
 
-    def _sfdx_extract(self):
-        # Create default package directory in temporary directory
-        self._temporary_package_directory = os.path.join(
-            self._temporary_directory, self._default_package_directory
-        )
-        self._retrieve_network()
-        self._set_experience_bundle_name()
 
-        self._retrieve_experience_bundle()
+class ReplaceThemeLayoutNavigationMenuTask(ExperienceBundleTask):
+
+    task_options = {
+        "navigation_menu": {
+            "description": "NavigationMenu API name to set for all theme layouts.",
+            "required": True,
+        },
+        **ExperienceBundleTask.task_options,
+    }
 
     def _sfdx_transform(self):
-        self._replace_theme_navigation_menus()
+        self._set_navigation_menu()
 
-    def _sfdx_load(self):
-        self._deploy_experience_bundle()
+    def _set_navigation_menu(self):
+        self._log_title("Set navigation menu")
+        self.logger.info(
+            'Setting the navigation menu as "{}" for all "{}" ExperienceBundle themes:'.format(
+                self.options["navigation_menu"], self._experience_bundle_name,
+            )
+        )
+
+        themes_path = os.path.join(
+            self._temporary_package_directory,
+            "main",
+            "default",
+            "experiences",
+            self._experience_bundle_name,
+            "themes",
+        )
+
+        for theme_file_dir_entry in os.scandir(themes_path):
+            if theme_file_dir_entry.is_file():
+                self.logger.info(f"  {theme_file_dir_entry.name}")
+
+                theme_file = Path(theme_file_dir_entry.path)
+
+                # Replace all "navigationMenuEditorRefresh" values with the navigation_menu option.
+                # This should be safe since it seems only the forceCommunity:themeNav component has a navigationMenuEditorRefresh attribute.
+                theme_file.write_text(
+                    re.sub(
+                        r'("navigationMenuEditorRefresh" ?: ?"\w*")',
+                        '"navigationMenuEditorRefresh" : "{}"'.format(
+                            self.options.get("navigation_menu")
+                        ),
+                        theme_file.read_text(),
+                        flags=re.M,
+                    )
+                )
